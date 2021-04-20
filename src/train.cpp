1d0f813ee45f96b963cc1fd6203b96db5f30c3ce
idrazzhivin <55536659+idrazzhivin@users.noreply.github.com>
2021-04-20 21:57:01 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -14,3 +14,36 @@ void Cage::off() {
 bool Cage::isLight() const {
   return lamp;
 }
+
+Train::Train()
+  : begin(nullptr), end(nullptr) {
+}
+
+void Train::addCage(Cage* _cage) {
+  if (nullptr == begin) {
+    _cage->right = _cage;
+    _cage->left = _cage;
+    begin = _cage;
+    end = _cage;
+  } else {
+    end->left = _cage;
+    _cage->right = end;
+    _cage->left = begin;
+    begin->right = _cage;
+    end = _cage;
+  }
+}
+
+
+
+
+void Train::print() {
+  Cage* curr = begin;
+  for (unsigned int step = 1; ; step++) {
+    std::cout << "Cage #" << step
+    << ", lamp is " << (curr->isLight() ? "on\n" : "off\n");
+    curr = curr->left;
+    if (curr == begin)
+      return;
+  }
+}
