5678bc7caec4605067197764bf83c222de0f4374
idrazzhivin <55536659+idrazzhivin@users.noreply.github.com>
2021-04-20 21:59:46 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -13,3 +13,26 @@ TEST(TrainTest, test2) {
   Cage* cage = new Cage(lamp);
   EXPECT_EQ(lamp, cage->isLight());
 }
+
+TEST(TrainTest, test3) {
+  bool lamp = std::rand() % 2;
+  Cage* cage = new Cage(lamp);
+  cage->on();
+  EXPECT_EQ(true, cage->isLight());
+}
+
+TEST(TrainTest, test4) {
+  bool lamp = std::rand() % 2;
+  Cage* cage = new Cage(lamp);
+  cage->off();
+  EXPECT_EQ(false, cage->isLight());
+}
+
+TEST(TrainTest, test5) {
+  Train* train = new Train;
+  train->createCages(25);
+  EXPECT_EQ(25, train->countLength());
+}
+
+
+
