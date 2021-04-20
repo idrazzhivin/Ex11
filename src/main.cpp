3341a0c66ea700d069f73f1b20c4e6156e7ea134
idrazzhivin <55536659+idrazzhivin@users.noreply.github.com>
2021-04-20 21:58:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,9 @@
 int main() {
   srand(time(0));
   Train* train1 = new Train;
+  train1->createCages(5); //set len to 5
+  train1->print(); 
+  std::cout << "The length of train is "
+  << train1->countLength() << std::endl;  // 10
   return 0;
 }
